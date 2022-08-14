fe0a4312068d623f0dd90f0074ed650466c87047
Markus Spath <markus@hackr.de>
2022-08-14 20:00:54 +0200

    update libs

--- app/src/main/java/com/example/androidbasics/courses/MainActivity.kt
+++ app/src/main/java/com/example/androidbasics/courses/MainActivity.kt
@@ -24,7 +24,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CoursesTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
